1b9671b217f8e32e9e69e56cebee453c5f346838
Zachary Zajdel <42820232+Zac-Zajdel@users.noreply.github.com>
2019-02-04 12:35:12 -0500

    Initial project setup

---
+++ react_chat_app/src/config.js
@@ -0,0 +1,4 @@
+export const tokenUrl = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/05a94c34-b6fb-4199-bc92-0578dfd2134a/token';
+
+export const instanceLocator = 'v1:us1:05a94c34-b6fb-4199-bc92-0578dfd2134a';
+
